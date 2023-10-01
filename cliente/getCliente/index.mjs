9cf6f1963b55a00a047ffa877e830b36cec02f2d
UADER-PEC <31574049+lu7did@users.noreply.github.com>
2023-09-30 21:16:27 -0300

    getCliente

--- cliente/getCliente/index.mjs
+++ cliente/getCliente/index.mjs
@@ -1,53 +1,49 @@
-  import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-	import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
-	import { GetItemCommand } from "@aws-sdk/client-dynamodb";
-	import { UpdateItemCommand } from "@aws-sdk/client-dynamodb";
+
+       //*------------------------------------------------------------------------------*
+        //* getCliente
+        //* API para acceder a un cliente dada su clave
+        //*
+        //* Dr. Pedro E. Colla
+        //* UADER - Ingeniería de Software I
+        //* (c) 2023 Dr. Pedro E. Colla
+        //*-----------------------------------------------------------------------------*
+        
+  import { DynamoDBClient, GetItemCommand } from "@aws-sdk/client-dynamodb";
 	import { randomUUID } from "crypto";
-	const ddbDocClient = DynamoDBDocumentClient.from(new DynamoDBClient({}));
+	import {marshall,unmarshall}  from "@aws-sdk/util-dynamodb";
+
+	const ddbClient = new DynamoDBClient(new DynamoDBClient({}));
 	export const handler = async (event, context) => {
 	    try {
 	        const cliente = JSON.parse(event.body);
-    	    
-    	    //*--- Obtiene fecha corriente y puebla campos relacionados
-    	    
-    	      var hoy = new Date();
-            var dd = String(hoy.getDate()).padStart(2, '0');
-            var mm = String(hoy.getMonth() + 1).padStart(2, '0'); //January is 0!
-            var yyyy = hoy.getFullYear();
-            hoy = dd + '/' + mm + '/' + yyyy;
-
 	        
-	 /*---- Recuperar el cliente desde la base ----*/
-	 
-            var input = {
-                    Key: { "id" : {"S" : cliente.id}},
-                    TableName : "cliente"
-            };
-                           
-            const command = new GetItemCommand(input);
-            const response = await ddbDocClient.send(command);
-            try {
-            	if (response.Item.id == 0) ;
-            } catch {
-  	          return {
-	               statusCode: 400,
-	               body: JSON.stringify({"response" : "Id no existe" }),
-	          }
+//*---- Arma clave de búsqueda
+
+         /*---- Recuperar el cliente desde la base ----*/
+
+          var input = {
+               Key: { "id" : {"S" : cliente.id}},
+               TableName : "cliente"
+          };
+
+          const command = new GetItemCommand(input);
+          const response = await ddbClient.send(command);
+          const uresponse = unmarshall(response.Item);
+          return {
+                 statusCode: 500,
+     	            body: JSON.stringify({ "data" : uresponse , "response" : "OK" }),
+          };
+          
+           
+//*----- Si algo anda mal retorna el error
+                                                     
+           } catch (error) {
+                console.error(error);
+                return {
+                    statusCode: 500,
+     	            body: JSON.stringify({ message: error.message , "response" : "Error" }),
+                };
             }
 
-	          return {
-	            statusCode: 200,
-	            body: JSON.stringify({"data" : response , "response" : "OK"}),
-	          }
-  
-	    } catch (error) {
-	        console.error(error);
-	        return {
-	            statusCode: 500,
-	            body: JSON.stringify({ message: error.message , "response" : "Error" }),
-	        };
-	        
-	    }
 };
-
 
